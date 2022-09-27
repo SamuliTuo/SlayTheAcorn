00700bf36cc0d354979cfff3a8f68331be159101
Samuli <samuli.tuomainen@gmail.com>
2022-09-27 14:23:17 +0300

    housekeeping

---
+++ Assets/Prefab/Potion.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewPotion", menuName = "ScriptableObjects/Potions", order = 1)]
+public class Potion : ScriptableObject
+{
+    public float damage = 1;
+    public StatusEffect statusEff = StatusEffect.NONE;
+    public int effectDurationInTurns = 1;
+}
