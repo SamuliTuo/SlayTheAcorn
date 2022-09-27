04b1d13ccf53c99e5d33e1b4a1993cc97be0d4f2
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2022-09-27 17:04:28 +0300

    Add bezier curve component & forest temp assets.

--- Assets/Scripts/Forest/PathCamera.cs
+++ Assets/Scripts/Forest/PathCamera.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PathCamera : MonoBehaviour
 {
+    public BezierCurve curve;
+    
+    public float phase = 0;
+    
+    public Vector3 shake;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +19,8 @@ public class PathCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        float t = Time.time * 2f;
+        this.transform.position = this.curve.GetPoint(this.phase)+ new Vector3( Mathf.Sin(t)*shake.x,Mathf.Sin(t*0.45f)*shake.y,Mathf.Sin(t*0.65f)*shake.z );
+        this.transform.LookAt(this.curve.GetPoint(this.phase)+new Vector3(0f,0f,10f) + this.curve.GetDirection(this.phase)*4f);
     }
 }
