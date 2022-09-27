2bd1c297d3ca2c3e4dc636fcccc5fda8af52c4b7
Samuli <samuli.tuomainen@gmail.com>
2022-09-27 13:37:12 +0300

    added enemy prefab

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,18 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GameObject canvas;
+    
     void Start()
     {
-        
+        canvas = GameObject.Find("Canvas");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void ThrowPotion()
+    {
+        print("POTIOOOOONES!");
     }
 }
