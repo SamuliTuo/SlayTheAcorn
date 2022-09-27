53a404da52512eeb6f3ce2158ae331d35258bbdb
Samuli <samuli.tuomainen@gmail.com>
2022-09-27 16:01:43 +0300

    making battle system

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public enum StatusEffect { NONE, CONFUSION, HALLUSINATION }
 public class EnemyController : MonoBehaviour
 {
-    
+    Dictionary<StatusEffect, int> currentStatuses = new Dictionary<StatusEffect, int>();
     float hp;
 
-    public bool ReceivePotion(
-        float healthAdd, 
-        StatusEffect statusEffect = StatusEffect.NONE, 
-        int statusEffectTurnDuration = 1
-        )
+    public bool ReceivePotionAttackAndCheckIfDead(Potion potion)
     {
-        hp += healthAdd;
+        print("AAAPUUUVAAAA");
+        if (potion.statusEff != StatusEffect.NONE)
+        {
+            currentStatuses.Add(potion.statusEff, potion.effectDurationInTurns);
+        }
+        
+        hp -= potion.damage;
         if (hp < 0)
             return true;
         else
             return false;
+    }
+
+    public void ApplyTurnEffects()
+    {
+        if (currentStatuses.Count > 0)
+        {
+            foreach (KeyValuePair<StatusEffect, int> status in currentStatuses)
+            {
+                //status.Value
+            }
+        }
     }
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,37 +3,123 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-
+public enum Turn { PLAYERTURN, ENEMYTURN }
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private PlayerInventory plrInventory = null;
     GameObject canvas;
-    EnemyController currentEnemy;
+    GameObject currentEnemy;
+    bool playerAlive;
+    bool enemyAlive;
+    Coroutine batteRoutine = null;
 
-    [SerializeField] private Potion potion_01 = null;
-    
-    
+    Turn currentTurn = Turn.PLAYERTURN;
+
+    Potion potionSlot_01;
+    Potion potionSlot_02;
+    Potion potionSlot_03;
+    Potion chosenPotion = null;
+
+    private GameObject enemy_01;
+    private GameObject enemy_02;
+    private GameObject enemy_03;
+    private GameObject enemy_04;
+    private GameObject enemy_05;
+    private GameObject enemy_06;
+    private GameObject enemy_07;
+    private GameObject enemy_08;
+    private GameObject enemy_09;
+    private GameObject enemy_10;
+
     void Start()
     {
         canvas = GameObject.Find("Canvas");
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    //void BattleStart(ene)
-    //{
-
-    //}
-
-    public void ThrowPotion()
-    {
-        if (currentEnemy == null)
+        enemy_01 = Resources.Load("Enemy_01") as GameObject;
+        if (batteRoutine == null)
         {
-            currentEnemy = canvas.transform.Find("Enemy").GetComponent<EnemyController>();
+            StartCoroutine(BattlePhase());
         }
-        currentEnemy.ReceivePotion(potion_01.damage, potion_01.statusEff, potion_01.effectDurationInTurns);
+    }
+
+    IEnumerator BattlePhase()
+    {
+        BattleStart(enemy_01);
+        yield return null;
+        while (playerAlive && enemyAlive)
+        {
+            if (currentTurn == Turn.PLAYERTURN)
+            {
+                if (chosenPotion == null)
+                {
+                    yield return null;
+                }
+
+                currentEnemy.GetComponent<EnemyController>().ReceivePotionAttackAndCheckIfDead(chosenPotion);
+                yield return new WaitForSeconds(2);
+            }
+
+            else if (currentTurn == Turn.ENEMYTURN)
+            {
+                currentEnemy.GetComponent<EnemyController>().ApplyTurnEffects();
+                yield return new WaitForSeconds(2);
+            }
+        }
+        print("Combat ended, player or enemy died");
+    }
+    
+
+
+    void BattleStart(GameObject enemy)
+    {
+        chosenPotion = null;
+        currentTurn = Turn.PLAYERTURN;
+        currentEnemy = enemy;
+        playerAlive = true;
+        enemyAlive = true;
+    }
+    
+    void ChangeTurns()
+    {
+        if (currentTurn == Turn.PLAYERTURN)
+        {
+            PlayerTurn();
+            currentTurn = Turn.ENEMYTURN;
+        }
+        else if (currentTurn == Turn.ENEMYTURN)
+        {
+            EnemyTurn();
+            currentTurn = Turn.PLAYERTURN;
+        }
+    }
+    void EnemyTurn()
+    {
+
+    }
+    void PlayerTurn()
+    {
+        potionSlot_01 = plrInventory.GetRandomPotion();
+        potionSlot_02 = plrInventory.GetRandomPotion();
+        potionSlot_03 = plrInventory.GetRandomPotion();
+    }
+
+
+
+    public void ThrowPotion_slot1()
+    {
+        chosenPotion = potionSlot_01;
+        plrInventory.AddPotion(potionSlot_02);
+        plrInventory.AddPotion(potionSlot_03);
+    }
+    public void ThrowPotion_slot2()
+    {
+        chosenPotion = potionSlot_02;
+        plrInventory.AddPotion(potionSlot_01);
+        plrInventory.AddPotion(potionSlot_03);
+    }
+    public void ThrowPotion_slot3()
+    {
+        chosenPotion = potionSlot_03;
+        plrInventory.AddPotion(potionSlot_01);
+        plrInventory.AddPotion(potionSlot_02);
     }
 }
