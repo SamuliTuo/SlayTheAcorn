db548f80e6680c3f10d489bb1ba87858c6af5314
Henri Sarasvirta <henri.sarasvirta@gmail.com>
2022-09-28 09:28:00 +0300

    cauldron drafting.

---
+++ Assets/Scripts/Cauldron/BrewResult.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrewResult : MonoBehaviour
+{
+    public Material firePotion;
+    public Material ghostPotion;
+    public Material heartbreakPotion;
+    public Material paralysisPotion;
+    public Material hallusinationPotion;
+    public Material shieldingPotion;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
